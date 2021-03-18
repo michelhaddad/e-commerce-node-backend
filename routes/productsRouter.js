2cfcfe04c8defd1bdaa88425c2c86372fe8c46c6
Michel Haddad <michhadd1@gmail.com>
2021-03-19 01:00:04 +0200

    Feat: enable uploading image for product given his id

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,6 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authenticate = require('../authenticate');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+const upload = multer({storage: storage});
 
 const Products = require('../models/products');
 
@@ -9,14 +23,36 @@ const productRouter = express.Router();
 
 productRouter.use(bodyParser.json());
 
-productRouter.route('/').
-    get((req, res, next) => {
-      Products.find({})
-          .then((products) => {
+productRouter.route('/:id/image').
+    put(upload.single('image'), (req, res, next) => {
+      const obj = {
+        data: fs.readFileSync(path.join(__dirname + '/../uploads/' + req.file.filename)),
+        contentType: 'image/png',
+      };
+      Products.findByIdAndUpdate(req.params.id, {$set: {img: obj}}, {new: true}).
+          then((product) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json(products);
+            res.json(product);
           }, (err) => next(err)).catch((err) => next(err));
+    }).
+    get((req, res, next) => {
+      Products.findById(req.params.id).
+          then((product) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'image/*');
+            res.send(product.img.data)
+          }, (err) => next(err)).
+          catch((err) => next(err));
+    });
+
+productRouter.route('/').
+    get((req, res, next) => {
+      Products.find({}).then((products) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(products);
+      }, (err) => next(err)).catch((err) => next(err));
     }).
     post(authenticate.verifyUser, authenticate.verifyAdmin,
         (req, res, next) => {
@@ -63,6 +99,6 @@ productRouter.route('/:id').
             res.setHeader('Content-Type', 'application/json');
             res.json(resp);
           }, (err) => next(err)).catch((err) => next(err));
-    });
+        });
 
 module.exports = productRouter;
