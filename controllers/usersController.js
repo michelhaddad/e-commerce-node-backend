a54a3bbf32dacf623fa9b938eebb375bac3f2215
jean-kesrewani <jean.kesrouani.intern@anghami.com>
2021-03-27 22:44:38 +0200

    refactored users handlers logic

---
+++ controllers/usersController.js
@@ -0,0 +1,51 @@
+const Users = require('../models/user');
+
+const findUser = (req, res, next) => {
+    Users.find()
+      .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+}
+
+const signUp = (req, res) => {
+    User.register(new User({... req.body}),
+      req.body.password, (err, user) => {
+        if(err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+        }
+        else {
+          user.admin = true;
+          user.save((err, user) => {
+            if (err) {
+              res.statusCode = 500;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({err: err});
+              return ;
+            }
+            passport.authenticate('local')(req, res, () => {
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({success: true, status: 'Registration Successful!'});
+            });
+          });
+        }
+      });
+};
+
+const logIn = (req, res) => {
+    const token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+}
+
+module.exports = {
+    findUser,
+    signUp,
+    logIn
+};
